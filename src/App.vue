1ce973d59df2d97efe55c76b656704f72b51bac7
Oscar Duran <xoscarduranx@gmail.com>
2017-01-13 16:29:08 +0100

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="phone-viewport">
+    <md-toolbar>
+      <md-button class="md-icon-button" @click="toggleLeftSidenav">
+        <md-icon>menu</md-icon>
+      </md-button>
+
+      <h2 class="md-title">My App</h2>
+    </md-toolbar>
+
+    <md-sidenav class="md-left" ref="leftSidenav" @open="open('Left')" @close="close('Left')">
+      <md-toolbar class="md-large">
+        <div class="md-toolbar-container">
+          <h3 class="md-title">Sidenav content</h3>
+        </div>
+      </md-toolbar>
+
+      <md-list>
+        <md-list-item>
+          <md-icon>move_to_inbox</md-icon> <span>Inbox</span>
+        </md-list-item>
+
+        <md-list-item>
+          <md-icon>send</md-icon> <span>Sent Mail</span>
+        </md-list-item>
+
+        <md-list-item>
+          <md-icon>delete</md-icon> <span>Trash</span>
+        </md-list-item>
+
+        <md-list-item>
+          <md-icon>error</md-icon> <span>Spam</span>
+
+          <md-divider class="md-inset"></md-divider>
+        </md-list-item>
+
+        <md-list-item>
+          <md-avatar>
+            <img src="https://placeimg.com/40/40/people/5" alt="People">
+          </md-avatar>
+
+          <span>Abbey Christansen</span>
+
+          <md-button class="md-icon-button md-list-action">
+            <md-icon class="md-primary">chat_bubble</md-icon>
+          </md-button>
+        </md-list-item>
+
+      </md-list>
+    </md-sidenav>
+
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  },
+  methods: {
+    toggleLeftSidenav () {
+      this.$refs.leftSidenav.toggle()
+    },
+    open (ref) {
+      console.log('Opened: ' + ref)
+    },
+    close (ref) {
+      console.log('Closed: ' + ref)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
